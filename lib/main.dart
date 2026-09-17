2c7eec6dabfdf6092f195a1279c96cd72b8bb766
Samba Sene <sambasene.sn@gmail.com>
2026-09-17 14:26:54 +0200

    fix: remove unused foundation.dart import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
+
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
